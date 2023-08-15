223d9f12118ab16dca8fd16357319aa2ca1c4174
Victor Rocha <victor.barbosa.rocha.dev@gmail.com>
2023-08-14 22:18:22 -0400

    changed something

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React Testing
+          Hello React Testing 2
         </p>
         <a
           className="App-link"
